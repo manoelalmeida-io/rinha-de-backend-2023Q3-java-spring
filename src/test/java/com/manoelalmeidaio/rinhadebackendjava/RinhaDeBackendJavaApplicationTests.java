ca78a1d18d94670bafd52b8ade26c53bfa723f43
Manoel Almeida <manoel.almeida@sptech.school>
2023-10-20 01:54:02 -0300

    Setup do projeto

---
+++ src/test/java/com/manoelalmeidaio/rinhadebackendjava/RinhaDeBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manoelalmeidaio.rinhadebackendjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhaDeBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
